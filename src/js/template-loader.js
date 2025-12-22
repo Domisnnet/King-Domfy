1d9ee958bd8ef21e961605acaf8787c4571aaba4
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-12-22 21:29:24 +0000

    identados

--- src/js/template-loader.js
+++ src/js/template-loader.js
@@ -4,15 +4,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Carrega o cabeçalho
   fetch(base + 'templates/header.html')
-      .then(response => response.text())
-      .then(data => {
-          document.getElementById('header-placeholder').innerHTML = data;
-      });
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header-placeholder').innerHTML = data;
+    });
 
   // Carrega o rodapé
   fetch(base + 'templates/footer.html')
-      .then(response => response.text())
-      .then(data => {
-          document.getElementById('footer-placeholder').innerHTML = data;
-      });
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer-placeholder').innerHTML = data;
+    });
 });
