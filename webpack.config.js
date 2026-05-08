7ec3de11b8c60f9db141ed06e4eaabb7a45d4da0
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-05-08 15:32:41 +0000

    ajustados

--- webpack.config.js
+++ webpack.config.js
@@ -1,117 +1,124 @@
 const path = require('path');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 const isDev = process.env.NODE_ENV !== 'production';
-
 const pages = [
-  "ajuda.html", "aplicativo-movel-gratis.html", "artistas.html", "baixar.html",
-  "cookies.html", "desenvolvedores.html", "empregos.html", "entrar.html",
-  "imprensa.html", "inscrever-se.html", "legal.html", "lgpd.html", "marcas.html",
-  "novidades.html", "player.html", "premium.html", "privacidade-termos.html",
-  "privacidade.html", "sobre.html", "suporte.html", "termos.html"
+  'ajuda.html',
+  'aplicativo-movel-gratis.html',
+  'artistas.html',
+  'baixar.html',
+  'cookies.html',
+  'desenvolvedores.html',
+  'empregos.html',
+  'entrar.html',
+  'imprensa.html',
+  'inscrever-se.html',
+  'legal.html',
+  'lgpd.html',
+  'marcas.html',
+  'novidades.html',
+  'player.html',
+  'premium.html',
+  'privacidade-termos.html',
+  'privacidade.html',
+  'sobre.html',
+  'suporte.html',
+  'termos.html',
 ];
 
-const htmlPlugins = pages.map(page =>
-  new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'src/pages', page),
+const headerPartial = fs.readFileSync( 
+  path.resolve(__dirname, 'src/partials/header.html'), 'utf8' );
+const footerPartial = fs.readFileSync( 
+  path.resolve(__dirname,  'src/partials/footer.html'), 'utf8' );
+const htmlPages = pages.map((page) => {
+  return new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, `src/pages/${page}`),
     filename: `pages/${page}`,
     inject: 'body',
+    scriptLoading: 'defer',
     minify: !isDev,
-  })
-);
+    templateParameters: {
+      header: headerPartial,
+      footer: footerPartial,
+    },
+  });
+});
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
   entry: path.resolve(__dirname, 'src/js/app.js'),
   output: {
-    filename: 'js/[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'js/main.[contenthash].js',
     publicPath: '/',
     clean: true,
+  },
+
+  devtool: isDev ? 'eval-source-map' : false,
+  devServer: {
+    static: { directory: path.resolve(__dirname, 'dist'), },
+    port: 3001, 
+    hot: true, 
+    open: true, 
+    compress: true, 
+    watchFiles: ['src/**/*'],
   },
 
   module: {
     rules: [
       {
         test: /\.html$/i,
-        exclude: /src\/pages/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              esModule: false, 
-              sources: {
-                list: ['...'],
-              },
-            },
-          },
-        ],
+        loader: 'html-loader',
+        options: {
+          esModule: false,
+          sources: { list: ['...'], },
+          minimize: false,
+        },
       },
       {
         test: /\.css$/i,
         use: [
-          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader',
         ],
       },
       {
         test: /\.(png|jpe?g|gif|svg|webp)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: 'assets/imagens/[name].[contenthash][ext]',
-        },
-      },
-      {
-        test: /\.(mp3|wav)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/media/[name].[contenthash][ext]',
-        },
+        generator: { filename: 'assets/imagens/[name].[contenthash][ext]', },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: 'assets/fonts/[name][ext]',
-        },
+        generator: { filename: 'assets/fonts/[name][ext]', },
+      },
+      {
+        test: /\.(mp3|wav)$/i,
+        type: 'asset/resource',
+        generator: { filename: 'assets/media/[name].[contenthash][ext]', },
       },
     ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/pages/home.html'),
+      template: path.resolve( __dirname, 'src/pages/home.html' ),
       filename: 'index.html',
       inject: 'body',
+      scriptLoading: 'defer',
       minify: !isDev,
+      templateParameters: { header: headerPartial, footer: footerPartial, },
     }),
-
-    ...htmlPlugins,
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash].css',
-    }),
-
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-    }),
+    ...htmlPages,
+    new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash].css', }),
+    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery', }),
   ],
 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      'jquery': path.resolve(__dirname, 'src/vendor/jquery/jquery.min.js'),
+      jquery: path.resolve( __dirname, 'src/vendor/jquery/jquery.min.js' ),
     },
-  },
-
-  devServer: {
-    port: 3001,
-    open: true,
-    hot: true,
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
-    },
-    watchFiles: ['src/**/*'],
   },
 };
