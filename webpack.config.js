e353e124dce2b5735a99791af9bf59e59badadc2
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-05-11 22:35:18 +0000

    refatorado

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpack = require('webpack');
 const isDev = process.env.NODE_ENV !== 'production';
 const pages = [
   'ajuda.html',
@@ -28,94 +27,64 @@ const pages = [
   'termos.html',
 ];
 
-const header = fs.readFileSync( path.resolve(__dirname, 'src/partials/header.html'), 'utf8' );
-const footer = fs.readFileSync( path.resolve(__dirname, 'src/partials/footer.html'), 'utf8' );
-const createPage = (template, output) =>
-  new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, template),
-    filename: output,
-    inject: 'body',
-    scriptLoading: 'defer',
-    minify: !isDev,
-  });
+const header = fs.readFileSync(path.resolve(__dirname, 'src/partials/header.html'), 'utf8');
+const footer = fs.readFileSync(path.resolve(__dirname, 'src/partials/footer.html'), 'utf8');
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
   entry: './src/js/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/main.[contenthash].js',
+    filename: 'js/[name].[contenthash].js',
     publicPath: '/',
     clean: true,
   },
-  devtool: isDev ? 'eval-source-map' : false,
   devServer: {
-    static: { directory: path.resolve(__dirname, 'dist'), },
+    static: path.resolve(__dirname, 'dist'),
     port: 3001,
     hot: true,
     open: true,
-    compress: true,
-    watchFiles: ['src/**/*'],
   },
-
   module: {
     rules: [
       {
         test: /\.html$/i,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              esModule: false,
-              minimize: false,
-              sources: {
-                list: [ '...', ],
-              },
-              preprocessor: (content) => {
-                return content
-                  .replace(/<%=\s*require\(['"]\.\.\/partials\/header\.html['"]\)\s*%>/g, header )
-                  .replace(/<%=\s*require\(['"]\.\.\/partials\/footer\.html['"]\)\s*%>/g, footer );
-              },
-            },
-          },
-        ],
+        use: [{
+          loader: 'html-loader',
+          options: {
+            preprocessor: (content) => content
+              .replace(/<id>header-placeholder<\/id>/g, header)
+              .replace(/<id>footer-placeholder<\/id>/g, footer)
+          }
+        }]
       },
       {
         test: /\.css$/i,
-        use: [
-          isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader',
-        ],
+        use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
         type: 'asset/resource',
-        generator: { filename: 'assets/imagens/[name].[contenthash][ext]', },
-      },
-      {
-        test: /\.(woff2?|ttf|eot|otf)$/i,
-        type: 'asset/resource',
-        generator: { filename: 'assets/fonts/[name][ext]', },
+        generator: { filename: 'assets/imagens/[name].[contenthash][ext]' },
       },
       {
         test: /\.(mp3|wav)$/i,
         type: 'asset/resource',
-        generator: { filename: 'assets/media/[name].[contenthash][ext]', },
+        generator: { filename: 'assets/media/[name].[contenthash][ext]' },
       },
     ],
   },
-
   plugins: [
-    createPage( 'src/pages/home.html', 'index.html' ),
-    ...pages.map((page) => createPage( `src/pages/${page}`, `pages/${page}` )),
-    new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash].css', }),
-    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery', }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/home.html',
+      filename: 'index.html',
+      inject: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/player.html',
+      filename: 'pages/player.html',
+      inject: true,
+    }),
+    new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash].css' }),
   ],
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      jquery: path.resolve(__dirname, 'src/vendor/jquery/jquery.min.js' ),
-    },
-  },
 };
