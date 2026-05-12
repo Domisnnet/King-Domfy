f3e49d27f4ba088931574584e827099fb7a05804
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-05-12 01:56:18 +0000

    ajustados

--- src/js/app.js
+++ src/js/app.js
@@ -3,29 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'vendor/fontawesome/css/all.min.css';
 import 'css/estilos-globais.css';
 
-import hino1 from 'assets/media/hino1.mp3';
-import hino2 from 'assets/media/hino2.mp3';
-import hino3 from 'assets/media/hino3.mp3';
-import hino4 from 'assets/media/hino4.mp3';
-import hino5 from 'assets/media/hino5.mp3';
-import hino6 from 'assets/media/hino6.mp3';
-import hino7 from 'assets/media/hino7.mp3';
-import hino8 from 'assets/media/hino8.mp3';
-import hino9 from 'assets/media/hino9.mp3';
-import hino10 from 'assets/media/hino10.mp3';
-
-import capaAlbum from 'assets/imagens/melodias-da-alma.jpeg';
-import img1 from 'assets/imagens/img1.jpeg';
-import img2 from 'assets/imagens/img2.jpeg';
-import img3 from 'assets/imagens/img3.jpeg';
-import img4 from 'assets/imagens/img4.jpeg';
-import img5 from 'assets/imagens/img5.jpeg';
-import img6 from 'assets/imagens/img6.jpeg';
-import img7 from 'assets/imagens/img7.jpeg';
-import img8 from 'assets/imagens/img8.jpeg';
-import img9 from 'assets/imagens/img9.jpeg';
-import img10 from 'assets/imagens/img10.jpeg';
-
 let indiceAtual = 1;
 const audio = new Audio();
 let isPlaying = false;
@@ -33,23 +10,22 @@ let isShuffle = false;
 let isRepeat = false;
 
 const playlist = [
-  { titulo: 'Capa do Álbum', caminho: hino1, capa: capaAlbum },
-  { titulo: 'Hino 1 - Hino da Jornada', caminho: hino1, capa: img1 },
-  { titulo: 'Hino 2 - A estrada é longa, mas eu sigo', caminho: hino2, capa: img2 },
-  { titulo: 'Hino 3 - Em Tua Vida, Deus', caminho: hino3, capa: img3 },
-  { titulo: 'Hino 4 - Siga adiante, não desanima', caminho: hino4, capa: img4 },
-  { titulo: 'Hino 5 - Eu só queria, Senhor!', caminho: hino5, capa: img5 },
-  { titulo: 'Hino 6 - Às vezes eu me sinto assim', caminho: hino6, capa: img6 },
-  { titulo: 'Hino 7 - O Sangue de Cristo Conhecido', caminho: hino7, capa: img7 },
-  { titulo: 'Hino 8 - O Amor aqui, esfriando está', caminho: hino8, capa: img8 },
-  { titulo: 'Hino 9 - Hoje é tão Difícil', caminho: hino9, capa: img9 },
-  { titulo: 'Hino 10 - Pelo Sangue purificado', caminho: hino10, capa: img10 },
+  { titulo: 'Capa do Álbum', capa: "assets/imagens/melodias-da-alma.jpeg"},
+  { titulo: 'Hino 1 - Hino da Jornada', caminho: "assets/media/hino1.mp3", capa: "assets/imagens/img1.jpeg" },
+  { titulo: 'Hino 2 - A estrada é longa, mas eu sigo', caminho: "assets/media/hino2.mp3", capa: "assets/imagens/img2.jpeg" },
+  { titulo: 'Hino 3 - Em Tua Vida, Deus', caminho: "assets/media/hino3.mp3", capa: "assets/imagens/img3.jpeg" },
+  { titulo: 'Hino 4 - Siga adiante, não desanima', caminho: "assets/media/hino3.mp3", capa: "assets/imagens/img3.jpeg" },
+  { titulo: 'Hino 5 - Eu só queria, Senhor!', caminho: "assets/media/hino5.mp3", capa: "assets/imagens/img5.jpeg" },
+  { titulo: 'Hino 6 - Às vezes eu me sinto assim', caminho: "assets/media/hino6.mp3", capa: "assets/imagens/img6.jpeg" },
+  { titulo: 'Hino 7 - O Sangue de Cristo Conhecido', caminho: "assets/media/hino7.mp3", capa: "assets/imagens/img7.jpeg" },
+  { titulo: 'Hino 8 - O Amor aqui, esfriando está', caminho: "assets/media/hino8.mp3", capa: "assets/imagens/img8.jpeg" },
+  { titulo: 'Hino 9 - Hoje é tão Difícil', caminho: "assets/media/hino9.mp3", capa: "assets/imagens/img9.jpeg" },
+  { titulo: 'Hino 10 - Pelo Sangue purificado', caminho: "assets/media/hino10.mp3", capa: "assets/imagens/img10.jpeg" },
 ];
 
 const initPlayer = () => {
   const btnPlayPause = document.getElementById('btnPlayPause');
   if (!btnPlayPause) return;
-
   const btnNext = document.getElementById('btnNext');
   const btnPrev = document.getElementById('btnPrev');
   const btnShuffle = document.getElementById('btnShuffle');
@@ -153,8 +129,7 @@ const initPlayer = () => {
     progressContainer.addEventListener('click', (e) => {
       if (!audio.duration) return;
       const width = progressContainer.clientWidth;
-      const clickX = e.offsetX;
-      audio.currentTime = (clickX / width) * audio.duration;
+      audio.currentTime = (e.offsetX / width) * audio.duration;
     });
   }
 
